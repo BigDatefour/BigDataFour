6303c813ebb466e679bd0f60503fcf7347287763
X-weijia <18535658809@163.com>
2018-09-12 01:15:02 +0800

    bd8

--- js/index2-xwj.js
+++ js/index2-xwj.js
@@ -17,7 +17,19 @@ off.click(function(){
     $(".alert_add").css("display","none");
 })
 
-
+let ass = $(".leftBox ul li a");
+ass.each(function(index,item){
+    $(item).click(function(){
+        ass.removeClass("active")
+        $("div").remove(".yuan")
+        console.log($(this))
+        $("<div>").addClass("yuan").appendTo(this).animate({
+            width:"300px",
+            height:"300px",
+        },1000)
+        $(this).addClass("active")
+    })
+})
 
 
 
