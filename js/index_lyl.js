56391fefb083c93a50861db0a56e5320fc19ad84
lin-xiaox <702397376@qq.com>
2018-09-21 08:48:19 +0800

    lyl2

--- js/index_lyl.js
+++ js/index_lyl.js
@@ -14,4 +14,19 @@ ass.each(function(index,item){
         $(this).addClass("active")
     })
 })
+
+
+//////////////////////////////顶部弹出框//////////////////////////////////
+let boxs = $(".projectile");
+let icon = $(".icon-down-trangle");
+// console.log(boxs,icon);
+icon.click(function(){
+    boxs.slideToggle()
+
 })
+
+
+
+
+})
+
